6fcbc2f2c68fb33a742d7d1a850e3fa6633b363d
monikmodi211 <55281872+monikmodi211@users.noreply.github.com>
2024-07-21 09:14:04 -0400

    Update to finish change data capture

--- datacapture/main.py
+++ datacapture/main.py
@@ -3,7 +3,7 @@ import psycopg2
 import random
 from datetime import datetime
 
-fake = faker.Fake()
+fake = faker.Faker()
 
 def generate_transaction():
     user= fake.simple_profile()
@@ -15,11 +15,13 @@ def generate_transaction():
         "timestamp": datetime.utcnow().timestamp(),
         "amount": round(random.uniform(10, 1000),2),
         "currency": random.choice(['USD','GBP']),
-        "merchantName": fake.compute(),
+        "city": fake.city(),
+        "country": fake.country(),
+        "merchantName": fake.company(),
         "paymentMethod": random.choice(['credit_card', 'debit_card','online_transfer']),
         "ipAddress": fake.ipv4(),
         "voucherCode": random.choice(['','DISCOUNT10','']),
-        "afiiliated Id": fake.uuid4()
+        "affiliateId": fake.uuid4()
     }
 
 def create_table(conn):
@@ -27,8 +29,8 @@ def create_table(conn):
 
     cursor.execute(
         """
-        CREATE TABLE IF NOT EXISTS transactions (
-            transaction_id VARCHAR(255),
+        CREATE TABLE IF NOT EXISTS transactions(
+            transaction_id VARCHAR(255) PRIMARY KEY,
             user_id VARCHAR(255),
             timestamp TIMESTAMP,
             amount DECIMAL,
@@ -37,13 +39,43 @@ def create_table(conn):
             country VARCHAR(255),
             merchant_name VARCHAR(255),
             payment_method VARCHAR(255),
-            ip_adress VARCHAR(255),
+            ip_address VARCHAR(255),
             voucher_code VARCHAR(255),
-            affiliateId VARCHAR(255)
+            affiliate_id VARCHAR(255)
         )
         """
     )
 
     cursor.close()
     conn.commit()
+
+if __name__ == "__main__":
+    conn = psycopg2.connect(
+        host = 'localhost',
+        database = 'financial_db',
+        user = 'postgres',
+        password = 'postgres',
+        port = 5432
+    )
+
+    create_table(conn)
+
+    transaction = generate_transaction()
+    cur =conn.cursor()
+    print(transaction)
+
+
+    cur.execute(
+        """
+        INSERT INTO transactions(transaction_id, user_id, timestamp, amount, currency, city, country, merchant_name, payment_method, ip_address, voucher_code, affiliate_id)
+        VALUES (%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        """, (transaction["transactionId"],transaction["userId"],datetime.fromtimestamp(transaction["timestamp"]).strftime('%Y-%m-%d %H:%M:%S'),transaction["amount"],transaction["currency"],transaction["city"],transaction["country"],transaction["merchantName"],
+        transaction["paymentMethod"],transaction["ipAddress"],transaction["voucherCode"],transaction["affiliateId"])
+    )
+
+    cur.close()
+    conn.commit()
+
+
+
 
